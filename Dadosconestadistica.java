a6f8362078efd8418b9d9de41d8b7885f8616b0f
vzlaranged <daph013@gmail.com>
2017-03-02 18:36:55 -0500

    Add files via upload

---
+++ Dadosconestadistica.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dadosconestadistica;
+
+/**
+ *
+ * @author lab20
+ */
+public class Dadosconestadistica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int ve[];
+        ve=apoyo.llenar();
+        apoyo.mostrar(ve);
+       
+    }
+}
